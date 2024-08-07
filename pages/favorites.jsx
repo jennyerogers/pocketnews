71da56969db8b841f647624b4c94223785be465a
Jenny Rogers <jrogers1@ufl.edu>
2024-08-07 10:47:10 -0400

    vercel testing

--- pages/favorites.jsx
+++ pages/favorites.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 import Footer from "../components/Footer";
 import { withIronSessionSsr } from "iron-session/next";
 import sessionOptions from "../config/session";
